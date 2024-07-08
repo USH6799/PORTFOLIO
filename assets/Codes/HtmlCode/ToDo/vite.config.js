88e0639251eea10996ddc823fc6b0b2bf4a4722e
USH6799 <165457875+USH6799@users.noreply.github.com>
2024-07-08 18:14:43 +0530

    a

--- assets/Codes/HtmlCode/ToDo/vite.config.js
+++ assets/Codes/HtmlCode/ToDo/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "PORTFOLIO/assets/Codes/HtmlCodes/ToDo",
+  base: "/PORTFOLIO/assets/Codes/HtmlCode/ToDo",
   plugins: [react()],
 })
